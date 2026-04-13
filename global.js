2530e9ecf1979e65ed0260bcd7f3f59151869b17
romyramrakhyani <romyramrakhyani19@gmail.com>
2026-04-13 17:40:46 -0600

    automated the current class

--- global.js
+++ global.js
@@ -7,3 +7,13 @@ console.log('IT’S ALIVE!');
 export function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+// Step 2.1: Get an array of all nav links
+let navLinks = $$("nav a");
+
+// Step 2.2: Find the link to the current page
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Step 2.3: Add the 'current' class to the current page link (using optional chaining)
+currentLink?.classList.add('current');
